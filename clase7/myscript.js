fa0a63b31cd364636f3611e4dcff1c45e18af343
Coty <52175595+geekcoty@users.noreply.github.com>
2019-12-06 16:34:13 -0300

    Add files via upload

--- clase7/myscript.js
+++ clase7/myscript.js
@@ -3,4 +3,9 @@ var nombrePersona= prompt("Como te llamas?")
 alert("Hola"+ nombrePersona)
 var edad= prompt("Que edad tenes?")
 var dias= edad*365
-alert("He vivido"+dias+"dias")
+alert("Has vivido"+dias+"dias")
+var miEdad=35
+var edadMaxima=90
+var snackFavorito="alfajores"
+var cantidadDiaria=2
+alert("Vas a necesitar"+cantidadDiaria*40150+snackFavorito+"para que te alcancen hasta los"+edadMaxima)
