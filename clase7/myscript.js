b9146fd100d22562b3b55f16d31cb28db166701e
Coty <52175595+geekcoty@users.noreply.github.com>
2019-12-06 16:08:21 -0300

    Add files via upload

--- clase7/myscript.js
+++ clase7/myscript.js
@@ -1,1 +1,6 @@
-alert ("Mi sitio es muy copado y deben disfrutarlo con cautela")
+alert("Mi sitio es muy copado y deben disfrutarlo con cautela")
+var nombrePersona= prompt("Como te llamas?")
+alert("Hola"+ nombrePersona)
+var edad= prompt("Que edad tenes?")
+var dias= edad*365
+alert("He vivido"+dias+"dias")
