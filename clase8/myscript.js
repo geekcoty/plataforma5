ddf78620410dcb48beed980f1dc80edd2a769c5f
Coty <52175595+geekcoty@users.noreply.github.com>
2019-12-06 21:44:48 -0300

    Add files via upload

--- clase8/myscript.js
+++ clase8/myscript.js
@@ -2,3 +2,7 @@ var respuesta=prompt("Como te sentis hoy?")
 if (respuesta==="enojada") {
   alert("Quema todo")
 }
+var numeroSecreto= prompt("Cual es el numero secreto?")
+if (numeroSecreto !==3){
+  alert("Te equivocaste!")
+}
