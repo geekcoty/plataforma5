95833f8790979faed5c44bf97bbf011eafb6fda3
Coty <52175595+geekcoty@users.noreply.github.com>
2019-12-06 21:54:54 -0300

    Add files via upload

--- clase8/myscript.js
+++ clase8/myscript.js
@@ -3,6 +3,14 @@ if (respuesta==="enojada") {
   alert("Quema todo")
 }
 var numeroSecreto= prompt("Cual es el numero secreto?")
-if (numeroSecreto !==3){
+if (numeroSecreto !=="3"){
   alert("Te equivocaste!")
+}
+numeroSecreto==="3"
+
+var password=prompt("Ingrese su password")
+if (password ==="gatites") {alert("Bienvenides!")
+
+} else {alert("You shall not pass!")
+
 }
