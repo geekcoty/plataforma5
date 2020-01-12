8d6fd7668f58541901ee7dd5977130ec8b808353
Coty <52175595+geekcoty@users.noreply.github.com>
2020-01-12 12:41:19 -0300

    Add files via upload

---
+++ clase9/myjs.js
@@ -0,0 +1,11 @@
+var input
+while(!(input = prompt('ya merito llegamos?'))){
+alert ("No escribiste nada")}
+if ((input==="Si") || (input==="SI")){input.toLowerCase(); alert ("Bienvenid@ a Muy Muy Lejano!")}
+else {alert ("Bienvenid@ a Muy Muy Lejano!")}
+
+var input
+while (!(input =prompt("Escribí un número"))){
+alert("No escribiste nada")}
+if (input > 0) {alert ("Escribiste" + input)}
+    else {alert ("Volvé a cargar la página")}
